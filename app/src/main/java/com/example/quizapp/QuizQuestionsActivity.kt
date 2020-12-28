bfc05c629ed018836cbe6339740306be23a055a2
simonw101a <simonw101@googlemail.com>
2020-12-28 22:41:42 +0000

    Initial commit

---
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,16 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quizapp.databinding.ActivityQuizQuestionsBinding
+
+class QuizQuestionsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityQuizQuestionsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityQuizQuestionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
